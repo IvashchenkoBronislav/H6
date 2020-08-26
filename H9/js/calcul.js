d4525dc341bb49e4fba6864b27e1f9d4d0960bd6
IvashchenkoBronislav <bronislav363@gmail.com>
2020-08-26 12:25:53 +0300

    H9
    На проверку

--- H9/js/calcul.js
+++ H9/js/calcul.js
@@ -15,33 +15,33 @@ document.querySelectorAll("button").forEach((button)=>{
             resultNum = num1 + num2;
             num1 = null;
             num2 = null;
-            input.value = resultNum;
+            input.placeholder = resultNum;
           break;
     
         case "-":
             resultNum = num1 - num2;
             num1 = null;
             num2 = null;
-            input.value = resultNum; 
+            input.placeholder=resultNum; 
             break;    
         case "*":
             resultNum = num1 * num2;
             num1 = null;
             num2 = null;
-            input.value = resultNum;    
+            input.placeholder= resultNum;    
             break;
     
         case "/":
             resultNum = num1 / num2;
             num1 = null;
             num2 = null;
-            input.value = resultNum;
+            input.placeholder=resultNum;
             break;     
 
         default:
             num1 = null;
             num2 = null;
-            input.value = resultNum;
+            input.placeholder=resultNum;
             break
       }
     }
@@ -55,21 +55,24 @@ document.querySelectorAll("button").forEach((button)=>{
             operator =null;
             resultNum=null;
             console.log("clin");
+            input.placeholder="0";
             
         }else if(button.value==="="||button.value==="-"||button.value==="+"||button.value==="/"||button.value==="*"||button.value==="%"){
                 if(num1 === null){
-                num1=parseFloat(input.value);
+                num1=input.value;
                 operator = button.value;
                 input.value="";
+                input.placeholder= num1 + operator;
+                    
             }else{
-                num2=parseFloat(input.value);
+                num2=input.value;
                 input.value=""
                 oper();
             }
                     
             
         }else{            
-            input.value += button.value;
+            input.value +=button.value;
             console.log("N");  
         }
 
@@ -81,4 +84,20 @@ document.querySelectorAll("button").forEach((button)=>{
         
         
     })    
+})
+
+let slid = document.querySelector(".calculator__button__dop__slid");
+let toggle = false;
+
+let buttonDop = document.querySelector(".calculator__button__dop");
+
+slid.addEventListener(("click"), ()=>{
+    buttonDop.classList.toggle('calculator__button__dop_vis');
+    if(toggle == false){
+        toggle=true;
+        slid.value = ">";
+    }else{
+        toggle=false;
+        slid.value ="<";
+    }
 })
